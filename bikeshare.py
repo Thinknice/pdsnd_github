b344320b9db5a1ff8d2ee61e198b2570fe3ffc5e
Thinknice <Thinknice81@yahoo.com>
2019-04-05 20:15:12 -0400

    refactor: Sort imports

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,8 @@
-import time
-import pandas as pd
-import numpy as np
 import json
+import time
+
+import numpy as np
+import pandas as pd
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
